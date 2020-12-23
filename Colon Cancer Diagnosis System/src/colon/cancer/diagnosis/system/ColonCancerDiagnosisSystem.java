16549190a9f5ab35abaa24afd6a3676c1d714a8e
Youssef Khaled <youssefkhaled001@outlook.com>
2020-12-23 21:53:18 +0200

    Intializing Project

---
+++ Colon Cancer Diagnosis System/src/colon/cancer/diagnosis/system/ColonCancerDiagnosisSystem.java
@@ -0,0 +1,14 @@
+
+package colon.cancer.diagnosis.system;
+
+
+public class ColonCancerDiagnosisSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
